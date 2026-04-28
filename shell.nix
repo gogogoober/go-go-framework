824b19e0675d187da295f5ba9655e390ee510bf4
Hugo Salinas <juicebox.salinas@gmail.com>
2026-04-28 17:46:31 -0500

    added nix shell

---
+++ shell.nix
@@ -0,0 +1,30 @@
+#nix-shell
+{pkgs ? import <nixpkgs> {}}:
+
+pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+    pkg-config
+    ];
+
+    buildInputs = with pkgs; [
+      go # pin the toolchain to nixpkgs
+      libGL # OpenGL loader
+      alsa-lib # audio
+      libX11 # X11 client x
+      libXcursor # cursor extension
+      libXrandr # display config
+      libXinerama # multi-monitor info
+      libXi # input extension
+      libXxf86vm # video mode extension 
+    ];
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+      libGL # runtime dlopen target
+      libX11 # belt and suspenders
+      libXcursor
+      libXrandr
+      libXinerama
+      libXi
+      libXxf86vm
+    ]);
+}
